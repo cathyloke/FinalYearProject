c180ef3fdc3671765d5b6773361934a5643d4034
cathyloke <catloke963@gmail.com>
2025-02-10 22:48:37 +0800

    Backup

--- app/screens/account/LoginScreen.tsx
+++ app/screens/account/LoginScreen.tsx
@@ -1,63 +1,112 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { TouchableOpacity, TextInput, Text, View, Image, StyleSheet, Dimensions } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from '../../assets/Types';
+import axios from 'axios'
 
 type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
 
 type Props = {
-  navigation: LoginScreenNavigationProp;
+    navigation: LoginScreenNavigationProp;
 };
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Hi, Ferianto!!</Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Email</Text>
-        <TextInput keyboardType='email-address' placeholder='Enter your email' placeholderTextColor="#C37BC3" style={styles.inputBox}></TextInput>
-        <Text style={styles.inputLabel}>Password</Text>
-        <TextInput keyboardType='visible-password' placeholder='Enter your password' placeholderTextColor="#C37BC3" style={styles.inputBox}></TextInput>
-      </View>
-      <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('Menu')}
-        >
-            <Text style={styles.buttonText}>Log in</Text>
-        </TouchableOpacity>
-        <Text style={{fontFamily: 'Itim-Regular'}}>Don't have an account?</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Register')}><Text style={{textDecorationLine: 'underline', fontFamily: 'Itim-Regular'}}>Register now!</Text></TouchableOpacity>
 
-        
-    </View>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginAccount = async () => {
+        try {
+            if (!email || !password) {
+                throw new Error('Missing login details')
+            }
+
+            const userData = {
+                email: email,
+                password: password
+            };
+
+            axios.post('http://192.168.1.17:3000/login', userData)
+                .then(res => {
+                    console.log('Successfully register : ' + res)
+                    console.log(JSON.stringify(res))
+                    navigation.navigate('Menu')
+                })
+                .catch(error => {
+                    console.error('Error:', error.message);
+                });
+
+        } catch (error) {
+            console.error('Error logging', error);
+            // setMessage('Error registering user.');
+        }
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Hi, Ferianto!!</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Email</Text>
+                <TextInput
+                    keyboardType="email-address"
+                    placeholder="Enter your email"
+                    placeholderTextColor="#C37BC3"
+                    style={styles.inputBox}
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                />
+                <Text style={styles.inputLabel}>Password</Text>
+                <TextInput
+                    keyboardType="visible-password"
+                    placeholder="Enter your password"
+                    placeholderTextColor="#C37BC3"
+                    style={styles.inputBox}
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    secureTextEntry
+                />
+
+            </View>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => loginAccount()}
+            >
+                <Text style={styles.buttonText}>Log in</Text>
+            </TouchableOpacity>
+            <Text style={{ fontFamily: 'Itim-Regular' }}>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}><Text style={{ textDecorationLine: 'underline', fontFamily: 'Itim-Regular' }}>Register now!</Text></TouchableOpacity>
+
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: '#F7EFE5',
         alignItems: 'center',
         justifyContent: 'center',
-        
+
     },
-    header:{
+    header: {
         fontSize: 40,
         fontFamily: 'Itim-Regular',
         color: 'black',
     },
-    inputContainer:{
+    inputContainer: {
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 30,
         marginBottom: 50,
     },
-    inputLabel:{
+    inputLabel: {
         fontSize: 25,
         fontFamily: 'Itim-Regular',
         color: 'black',
     },
-    inputBox:{
+    inputBox: {
         fontFamily: 'Itim-Regular',
         color: '#C37BC3',
         fontSize: 20,
@@ -65,32 +114,32 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderColor: 'black',
         borderRadius: 40,
-        borderWidth: 2, 
+        borderWidth: 2,
         width: 270,
         height: 65,
     },
-    button:{
-        alignItems: 'center', 
+    button: {
+        alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#C37BC3',
-        width: 250, 
+        width: 250,
         height: 65,
         marginBottom: 20,
-        borderRadius: 40,               
+        borderRadius: 40,
         elevation: 15,                          //android
         shadowColor: '#000',                    //ios
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.8,
         shadowRadius: 3.84,
     },
-    buttonText:{
+    buttonText: {
         fontFamily: 'Itim-Regular',
         justifyContent: 'center',
         color: 'white',
         alignSelf: 'center',
         fontSize: 30,
     },
-    
+
 });
 
 export default LoginScreen;
--- app/screens/account/RegisterScreen.tsx
+++ app/screens/account/RegisterScreen.tsx
@@ -1,85 +1,178 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { TouchableOpacity, TextInput, Text, View, StyleSheet } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from '../../assets/Types';
 //import { CheckBox } from 'react-native-elements';
 import CheckBox from "expo-checkbox";
+import { ScrollView } from "react-native-gesture-handler";
+import axios from 'axios'
+
 type RegisterScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Register'>;
 
 type Props = {
-  navigation: RegisterScreenNavigationProp;
+    navigation: RegisterScreenNavigationProp;
 };
 
 const RegisterScreen: React.FC<Props> = ({ navigation }) => {
 
     const [isChecked, setIsChecked] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repassword, setRepassword] = useState('');
+    const [message, setMessage] = useState('');
 
     const handleCheckBoxChange = () => {
-      setIsChecked(!isChecked);
-      navigation.navigate('TNC');
+        setIsChecked(!isChecked);
+        navigation.navigate('TNC');
     };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.header}>Hi, Ferianto!!</Text>
-            <View style={styles.inputContainer}>
-                <Text style={styles.inputLabel}>Email</Text>
-                <TextInput keyboardType='email-address' placeholder='Enter your email' placeholderTextColor="#C37BC3" style={styles.inputBox}></TextInput>
-                <Text style={styles.inputLabel}>Password</Text>
-                <TextInput keyboardType='visible-password' placeholder='Enter your password' placeholderTextColor="#C37BC3" style={styles.inputBox}></TextInput>
-                <Text style={styles.inputLabel}>Re-Password</Text>
-                <TextInput keyboardType='visible-password' placeholder='Re-Enter your password' placeholderTextColor="#C37BC3" style={styles.inputBox}></TextInput>
-            </View>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate('Menu')}
-            >
-                <Text style={styles.buttonText}>Register</Text>
-            </TouchableOpacity>
-            <Text style={{fontFamily: 'Itim-Regular'}}>Already have an account?</Text>
-            <TouchableOpacity 
-                onPress={() => navigation.navigate('Login')}
-            >
-                <Text style={[{textDecorationLine: 'underline'}, {fontFamily: 'Itim-Regular'}]}>Log in now!</Text>
-            </TouchableOpacity>
+    const isValidEmail = (email: any) => {
+        // Regular expression for basic email validation
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        return emailRegex.test(email);
+    };
 
-            <View style={styles.checkboxContainer}>
-                <CheckBox
-                    value={isChecked}
-                    onValueChange={handleCheckBoxChange}
-                    color={isChecked ? '#C8A1E0': 'grey'}
-                >   
-                </CheckBox>
-                <Text style={styles.checkboxText}>I had read the <Text style={{textDecorationLine: 'underline'}} onPress={handleCheckBoxChange}>terms and conditions</Text></Text>
+    const registerAccount = async () => {
+        try {
+            if (password !== repassword) {
+                throw new Error('Password not the same')
+            }
+
+            if (!isValidEmail(email)) {
+                throw new Error('Email is not valid')
+            }
+
+            const userData = {
+                name: name,
+                email: email,
+                password: password
+            };
+
+
+            axios.post('http://192.168.1.17:3000/register', userData)
+                .then(res => {
+                    console.log('Successfully register : ' + res)
+                    console.log(JSON.stringify(res))
+                    navigation.navigate('Menu')
+                })
+                .catch(error => {
+                    console.error('Error:', error.message);
+                });
+
+
+
+
+        } catch (error) {
+            console.error('Error registering user:', error);
+            setMessage('Error registering user.');
+        }
+
+    }
+
+
+    return (
+        <ScrollView style={styles.container}>
+            <View style={{
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <Text style={styles.header}>Hi, Ferianto!!</Text>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.inputLabel}>Name</Text>
+                    <TextInput
+                        keyboardType='email-address'
+                        placeholder='Enter your name'
+                        placeholderTextColor="#C37BC3"
+                        style={styles.inputBox}
+                        value={name}
+                        onChangeText={text => setName(text)}
+                    >
+                    </TextInput>
+                    <Text style={styles.inputLabel}>Email</Text>
+                    <TextInput
+                        keyboardType="email-address"
+                        placeholder="Enter your email"
+                        placeholderTextColor="#C37BC3"
+                        style={styles.inputBox}
+                        value={email}
+                        onChangeText={text => setEmail(text)}
+                    />
+                    <Text style={styles.inputLabel}>Password</Text>
+                    <TextInput
+                        keyboardType="visible-password"
+                        placeholder="Enter your password"
+                        placeholderTextColor="#C37BC3"
+                        style={styles.inputBox}
+                        value={password}
+                        onChangeText={text => setPassword(text)}
+                        secureTextEntry
+                    />
+                    <Text style={styles.inputLabel}>Re-Password</Text>
+                    <TextInput
+                        keyboardType="visible-password"
+                        placeholder="Re-Enter your password"
+                        placeholderTextColor="#C37BC3"
+                        style={styles.inputBox}
+                        value={repassword}
+                        onChangeText={text => setRepassword(text)}
+                        secureTextEntry
+                    />
+
+                </View>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => registerAccount()}
+                >
+                    <Text style={styles.buttonText}>Register</Text>
+                </TouchableOpacity>
+                {/* {message && <Text>{message}</Text>} */}
+
+                <Text style={{ fontFamily: 'Itim-Regular' }}>Already have an account?</Text>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Login')}
+                >
+                    <Text style={[{ textDecorationLine: 'underline' }, { fontFamily: 'Itim-Regular' }]}>Log in now!</Text>
+                </TouchableOpacity>
+
+                <View style={styles.checkboxContainer}>
+                    <CheckBox
+                        value={isChecked}
+                        onValueChange={handleCheckBoxChange}
+                        color={isChecked ? '#C8A1E0' : 'grey'}
+                    >
+                    </CheckBox>
+                    <Text style={styles.checkboxText}>I had read the <Text style={{ textDecorationLine: 'underline' }} onPress={handleCheckBoxChange}>terms and conditions</Text></Text>
+                </View>
             </View>
-        </View>
+
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: '#F7EFE5',
-        alignItems: 'center',
-        justifyContent: 'center',  
+
     },
-    header:{
+    header: {
         fontSize: 40,
         fontFamily: 'Itim-Regular',
         color: 'black',
     },
-    inputContainer:{
+    inputContainer: {
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 10,
         marginBottom: 20,
     },
-    inputLabel:{
+    inputLabel: {
         fontSize: 25,
         fontFamily: 'Itim-Regular',
         color: 'black',
     },
-    inputBox:{
+    inputBox: {
         fontFamily: 'Itim-Regular',
         color: '#C37BC3',
         fontSize: 20,
@@ -87,25 +180,25 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderColor: 'black',
         borderRadius: 40,
-        borderWidth: 2, 
+        borderWidth: 2,
         width: 270,
         height: 65,
     },
-    button:{
-        alignItems: 'center', 
+    button: {
+        alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#C37BC3',
-        width: 250, 
+        width: 250,
         height: 65,
         marginBottom: 20,
-        borderRadius: 40,             
+        borderRadius: 40,
         elevation: 15,                          //android
         shadowColor: '#000',                    //ios
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.8,
         shadowRadius: 3.84,
     },
-    buttonText:{
+    buttonText: {
         fontFamily: 'Itim-Regular',
         justifyContent: 'center',
         color: 'white',
@@ -117,9 +210,9 @@ const styles = StyleSheet.create({
     },
     checkboxText: {
         paddingLeft: 5,
-        fontSize: 15, 
+        fontSize: 15,
         fontFamily: 'Itim-Regular',
-        color: 'black', 
+        color: 'black',
     },
 });
 
